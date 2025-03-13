d0a347798e1eb37b6a28e4a69ad7566a1391c63d
Murad Kamil <muradkamilllll@gmail.com>
2025-03-14 00:21:03 +0400

    Initial commit

--- src/app/enviroment/enviroment.ts
+++ src/app/enviroment/enviroment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-       production: false,
-       youtubeApiKey: 'AIzaSyDvTFMwDYWeUTPCbQwpGI-vaj4FfGz37EI',
-       omdbApiKey: '6206dff2'
-     };
+  production: false,
+  youtubeApiKey: 'AIzaSyDvTFMwDYWeUTPCbQwpGI-vaj4FfGz37EI',
+  omdbApiKey: '6206dff2',
+};
